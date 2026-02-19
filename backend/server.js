1f411654d2aca42839f42e4e2541750836aae6f8
Zsombor0818 <150193792+Zsombor0818@users.noreply.github.com>
2026-02-19 11:17:38 +0100

    Backend server mukodo

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const app = express();
+
+const SECRET_KEY = "szupertitkoskulcs"; 
+
+const path = require('path');
+
+app.set('views', path.join(__dirname, '../frontend', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '../frontend', 'public')));
+
+app.get('/', async (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => console.log('Szerver fut: http://localhost:3000'));
